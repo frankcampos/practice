30e44444527d1365d16b5d4e55df2fb78604b914
frank <frankcampos85@gmail.com>
2023-09-01 22:22:32 -0500

    OR, alternation operator |

--- Regular Expression.js
+++ Regular Expression.js
@@ -1,4 +1,12 @@
-let mytext = 'my name is frank';
-let name = /frank/;
+let mytext = 'my name is frank walberto Parada Campos';
+let name = /frank/; //literal match
 let findMyname = name.test(mytext);
 console.log(findMyname);
+
+
+// OR operator, alternation operator
+// match one or more patterns of characters wanted
+
+let moreWords= /frank| walberto| Campos/;
+let completedName = moreWords.test(mytext);
+console.log(completedName);
