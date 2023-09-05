b009f7f70770fe5ec159f1a235a85e5be319584a
frank <frankcampos85@gmail.com>
2023-09-05 18:18:11 -0500

    match() method

--- Regular Expression.js
+++ Regular Expression.js
@@ -1,14 +1,25 @@
 let mytext = 'my name is frank walberto Parada Campos';
-let name = /frank/; //literal match
-let findMyname = name.test(mytext);
+let myName= /frank/; //literal match
+let findMyname = myName.test(mytext);
 console.log(findMyname);
 
 
 // OR operator, alternation operator
 // match one or more patterns of characters wanted
 
-let moreWords= /frank| walberto| Campos/;
+let moreWords= /frank| walberto| Campos/; // add more word to regular expression variable MoreWords.
 let completedName = moreWords.test(mytext);
 console.log(completedName);
 
-//new key ssh
+//Ignore case while matching
+let greenting = 'HeLLo';
+let greetingRegex =/hello/i;// even if a work has a diferent case, It will match 
+let  checkCompat = greetingRegex.test(greenting);
+console.log(checkCompat);
+
+// method match()
+let myStr = 'love is powerful';
+let myRegex = /love/i;
+let myMatch = myStr.match(myRegex);
+console.log(myMatch);
+
