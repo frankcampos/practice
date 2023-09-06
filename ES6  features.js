6599131c3f5f8626696190b88798d3696110bf11
frank <frankcampos85@gmail.com>
2023-09-05 22:16:04 -0500

    destructuring assignment object and array

---
+++ ES6  features.js
@@ -0,0 +1,18 @@
+const myCar ={ brand :'Ram',
+model: {year:2012,
+    color: 'blue',
+    motor:"v8"}
+
+};
+
+// Destructuring Assignment 
+// extract properties of the object into variables 
+
+
+const {brand, model:{year,color,motor} }= myCar; // no need of '=' sing 
+console.log(brand,year,color,motor);
+
+const myShoppingList = ['water', 'shampoo', 'chicken', 'motoroil'];
+//extracting values from an array into variable
+const [water, shampoo, chicken] = myShoppingList;
+console.log(water,shampoo,chicken);
